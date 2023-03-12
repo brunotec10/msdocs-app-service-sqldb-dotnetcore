a6a53c206c13977e0c7e4ae57da42d000a10c840
brunotec10 <124909048+brunotec10@users.noreply.github.com>
2023-03-11 22:56:22 -0500

    desactivando certi

--- DotNetCoreSqlDb/Startup.cs
+++ DotNetCoreSqlDb/Startup.cs
@@ -53,7 +53,7 @@ namespace DotNetCoreSqlDb
 
             app.UseAuthorization();
             
-            #region csp
+            /*#region csp
             app.Use(async (context, next) =>
             {
                 context.Response.Headers.Add("Content-Security-Policy", "default-src 'self' *.azurewebsites.net; style-src 'self' https://stackpath.bootstrapcdn.com/; script-src 'Self' 'unsafe-inline' https://stackpath.bootstrapcdn.com/ https://cdnjs.cloudflare.com; img-src data:");
