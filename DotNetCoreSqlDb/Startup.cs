ae6d4f116a43979644a7f71a2c5c91f8959b287d
brunotec10 <124909048+brunotec10@users.noreply.github.com>
2023-05-01 23:15:04 -0500

    prueba 2

--- DotNetCoreSqlDb/Startup.cs
+++ DotNetCoreSqlDb/Startup.cs
@@ -56,7 +56,7 @@ namespace DotNetCoreSqlDb
             #region csp
             app.Use(async (context, next) =>
             {
-                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; style-src 'self' https://stackpath.bootstrapcdn.com/; script-src 'Self' 'unsafe-inline' https://stackpath.bootstrapcdn.com/ https://cdnjs.cloudflare.com 'sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=' 'sha256-fzFFyH01cBVPYzl16KT40wqjhgPtq6FFUB6ckN2+GGw=' ; img-src data:;frame-ancestors 'none'; form-action 'self'");
+                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; style-src 'self' https://stackpath.bootstrapcdn.com/; script-src 'Self' 'sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=' 'sha256-fzFFyH01cBVPYzl16KT40wqjhgPtq6FFUB6ckN2+GGw=' ; img-src data:;frame-ancestors 'none'; form-action 'self'");
                 await next();
             });
             #endregion
