fba80b1a41d98a631630e87f1b5b961624abec6f
brunotec10 <124909048+brunotec10@users.noreply.github.com>
2024-03-11 21:43:23 -0500

    Borrando codigo

--- DotNetCoreSqlDb/Startup.cs
+++ DotNetCoreSqlDb/Startup.cs
@@ -53,7 +53,7 @@ namespace DotNetCoreSqlDb
 
             app.UseAuthorization();
             
-            #region csp
+           /** #region csp
             app.Use(async (context, next) =>
             {
                 context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; style-src 'self' https://stackpath.bootstrapcdn.com/; script-src 'Self' 'unsafe-inline' https://stackpath.bootstrapcdn.com/ https://cdnjs.cloudflare.com 'sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=' 'sha256-fzFFyH01cBVPYzl16KT40wqjhgPtq6FFUB6ckN2+GGw=' ; img-src data:;frame-ancestors 'none'; form-action 'self'");
@@ -68,7 +68,7 @@ namespace DotNetCoreSqlDb
                 Secure = CookieSecurePolicy.Always
             });
             #endregion
-
+            **/
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
