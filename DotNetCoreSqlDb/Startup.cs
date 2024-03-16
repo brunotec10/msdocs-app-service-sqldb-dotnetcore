c393b5d5f5a27a87d2b10f128f934b84d563eb92
brunotec10 <124909048+brunotec10@users.noreply.github.com>
2024-03-16 17:07:50 -0500

    data

--- DotNetCoreSqlDb/Startup.cs
+++ DotNetCoreSqlDb/Startup.cs
@@ -56,7 +56,7 @@ namespace DotNetCoreSqlDb
            #region csp
             app.Use(async (context, next) =>
             {
-                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; style-src 'self' https://stackpath.bootstrapcdn.com/; script-src 'Self' 'unsafe-inline' https://stackpath.bootstrapcdn.com/ https://cdnjs.cloudflare.com ; img-src 'self' data:image/svg+xml");
+                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; style-src 'self' https://stackpath.bootstrapcdn.com/; script-src 'Self' 'unsafe-inline' https://stackpath.bootstrapcdn.com/ https://cdnjs.cloudflare.com ; img-src 'self' data:");
                 await next();
             });
             #endregion
